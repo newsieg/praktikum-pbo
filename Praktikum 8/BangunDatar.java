a4fa4b49813e82b624404a4cc456139bfcc92efe
newsieg <tsurayaolivia@gmail.com>
2025-05-14 22:29:31 +0700

    Create BangunDatar.java

---
+++ Praktikum 8/BangunDatar.java
@@ -0,0 +1,10 @@
+/************************************/
+/* Program   : BangunDatar.java */
+/* Deskripsi : Kelas BangunDatar*/
+/* NIM/Nama  : 24060123130114/Tsuraya Olivia*/
+/* Tanggal   : 8 Mei 2025*/
+/***********************************/
+
+public abstract class BangunDatar {
+    public abstract double hitungKeliling();
+}
